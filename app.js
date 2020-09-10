223d6a9a2b3ee926b0816637fc11d35223789f50
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-10 23:38:07 +0530

    Returning a Form using request url

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const http = require('http'); //const because we are not gonna change it, reuire
 
 
 const server = http.createServer((req , res) => {   // this will be called when ever a request reaches node js server
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form acton="/messages" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();        
+    }
     //process.exit();
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello User</h1></body>');
     res.write('</html>');
+    res.end();
 });// request listner recives a request and gives us a object response to return a response  
 
 //oon running it will give nothing beacause createserver return a server so we have to store it and then listen it,listen will not terminate the code and will wait and listen  
