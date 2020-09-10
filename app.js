5d21fd9705a6cefcc64a6ebb20becf6cf9818e82
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-10 23:07:48 +0530

    returning html content

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const http = require('http'); //const because we are not gonna change it, reuire
 
 
 const server = http.createServer((req , res) => {   // this will be called when ever a request reaches node js server
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello User</h1></body>');
+    res.write('</html>');
 });// request listner recives a request and gives us a object response to return a response  
 
 //oon running it will give nothing beacause createserver return a server so we have to store it and then listen it,listen will not terminate the code and will wait and listen  
