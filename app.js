9e7844a2fe449afcfbdc21801cd58d885a0af1c5
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-10 22:05:30 +0530

    creating server

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http'); //const because we are not gonna change it, reuire is simply a way how you import file in js and http will search for global if i would have written / or ./ http then it would search file in local
+
+
+const server = http.createServer((req , res) => {   // this will be called when ever a request reaches node js server
+    console.log(req);
+});// request listner recives a request and gives us a object response to return a response  
+
+//oon running it will give nothing beacause createserver return a server so we have to store it and then listen it,listen will not terminate the code and will wait and listen  
+server.listen(3000);
