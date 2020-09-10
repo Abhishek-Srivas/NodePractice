4d20f26be9df3e6db03d5394b2f1319fea46c290
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-11 00:16:12 +0530

    Getting Dummy Data

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const http = require('http'); //const because we are not gonna change it, reuire is simply a way how you import file in js and http will search for global if i would have written / or ./ http then it would search file in local
+const fs = require('fs'); //contain all inbuild file func and obj
 
 
 const server = http.createServer((req , res) => {   // this will be called when ever a request reaches node js server
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form acton="/messages" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();        
     }
     //process.exit();
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt','Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
