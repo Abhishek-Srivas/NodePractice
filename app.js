e9bb3b79f9503eb0b9cf984dcc948d770f23ffb5
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-12 11:17:17 +0530

    added body-parser

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const http = require('http'); //const because we are not gonna change it, reuire is simply a way how you import file in js and http will search for global if i would have written / or ./ http then it would search file in local
+//const http = require('http'); //const because we are not gonna change it, reuire is simply a way how you import file in js and http will search for global if i would have written / or ./ http then it would search file in local
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( (req, res, next) =>{
+/* app.use( (req, res, next) =>{
     console.log("in the middleware");
     next(); // Allow the req to continue to the next middleware
+})*/
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product',(req, res, next) => {
+    res.redirect('/');
 })
 
 app.use( (req, res, next) => {
